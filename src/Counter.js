8448cae0b35b6f7437c808d58fe75e5572e09a01
Shivraj97 <43899907+Shivraj97@users.noreply.github.com>
2020-12-05 20:22:56 +0530

    decrement fixed

--- src/Counter.js
+++ src/Counter.js
@@ -36,7 +36,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     increment: () => dispatch(increment()),
-    decrement: () => dispatch(increment())
+    decrement: () => dispatch(decrement())
   };
 };
 
